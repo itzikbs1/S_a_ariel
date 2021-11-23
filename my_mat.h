68c3418d1cdc0c1d88e9d6971d528afa6cc6ba24
itzikbs1 <92460446+itzikbs1@users.noreply.github.com>
2021-11-23 18:37:26 +0200

    Update my_mat.h

--- my_mat.h
+++ my_mat.h
@@ -1,9 +1,8 @@
-// #ifndef _MY_MAT_H_
-// #define _MY_MAT_H_
-#define N 10
-void valueOfTheArray(int mat[N][N]);
-void checkPath(int mat [N][N]);
-void smallestPath(int mat [N][N]);
-int min(int a, int b);
+#ifndef _MY_MAT_H_
+#define _MY_MAT_H_
 
-// #endif
+void getMat();
+void route();
+void fastestRoute();
+
+#endif
