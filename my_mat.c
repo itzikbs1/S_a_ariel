0dcdedacc11386dfab1645c0f7d6b9c4dc9d40ef
itzikbs1 <92460446+itzikbs1@users.noreply.github.com>
2021-11-23 18:43:55 +0200

    Update my_mat.c

--- my_mat.c
+++ my_mat.c
@@ -25,7 +25,7 @@ void route(){
     scanf("%d %d" , &i, &j);
     for (k = 0; k < n; k++){
         for (x = 0; x < n; x++){
-            arr[k][x] = mat[k][x];
+            arr[k][x] = m[k][x];
         }
     }
         if (arr[k][x] == 0 && k != x){
@@ -45,17 +45,19 @@ void route(){
 }
 
 void buildFastestRoutes(int y, int z){
+    int k,x, i, j;
+    int arr[n][n];
     for (k = 0; k < n; k++){
         for (x = 0; x < n; x++){
-            arr[k][x] = mat[k][x];
+            arr[k][x] = m[k][x];
         }
     }
         if (arr[k][x] == 0 && k != x){
             arr[k][x] = MaxValue;
         }
     for (int k = 0; k < n; k++){
-            for (int i = 0; i < n; i++){
-                for (int j = 0; j < n; j++){
+            for (i = 0; i < n; i++){
+                for (j = 0; j < n; j++){
                     m[i][j] = findMin(m[i][j], m[i][k] + m[k][j]);
                 }
             }
